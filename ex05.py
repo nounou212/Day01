77a1b87750f11c21ce5cb43f4bc79655a9461f0d
Nawel Wendling <nawelwendling@MacBook-Pro-de-Nawel.local>
2025-01-06 16:09:02 +0100

    Add ex05

---
+++ ex05.py
@@ -0,0 +1,8 @@
+# On crée une liste de quelques éléments
+ma_liste = [5, 6, 7, 8, 9]
+
+# On affiche la liste d'origine
+print("Liste d'origine :", ma_liste)
+
+# On affiche la liste inversée
+print("Liste inversée  :", ma_liste[::-1])
